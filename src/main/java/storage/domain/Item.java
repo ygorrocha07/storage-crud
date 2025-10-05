a1f0f344ebd702022454ca924c7322a24c75dc4d
ygorrocha07 <ygor.rocha07@outlook.com>
2025-10-05 20:18:09 -0300

    Base do projeto

---
+++ src/main/java/storage/domain/Item.java
@@ -0,0 +1,41 @@
+package storage.domain;
+
+public class Item {
+    String nome;
+    Integer quantidade;
+    Double valor;
+
+    public Item() {
+
+    }
+
+    public Item(String nome, Integer quantidade, Double valor) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.valor = valor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+}
